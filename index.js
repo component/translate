0be92d78ffd9e183f995cb5fff9d71b5db9af2e5
mnmly <i.am@mnmly.com>
2013-09-19 07:24:19 -0700

    Add support for percentage value

--- index.js
+++ index.js
@@ -12,21 +12,28 @@ var has3d = require('has-translate3d');
 
 module.exports = translate;
 
+var percentEndPattern = /%$/;
+
 /**
  * Translate `el` by `(x, y)`.
  *
  * @param {Element} el
- * @param {Number} x
- * @param {Number} y
+ * @param {Number|String} x Pixel value in number of percent value
+ * @param {Number|String} y 
  * @api public
  */
 
+
 function translate(el, x, y){
+  
+  if (!percentEndPattern.test(x)) x += 'px';
+  if (!percentEndPattern.test(y)) y += 'px';
+
   if (transform) {
     if (has3d) {
-      el.style[transform] = 'translate3d(' + x + 'px,' + y + 'px, 0)';
+      el.style[transform] = 'translate3d(' + x + ', ' + y + ', 0)';
     } else {
-      el.style[transform] = 'translate(' + x + 'px,' + y + 'px)';
+      el.style[transform] = 'translate(' + x + ',' + y + ')';
     }
   } else {
     el.style.left = x;
